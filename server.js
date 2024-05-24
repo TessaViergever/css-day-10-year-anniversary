652d43e9ebbf24b6ed67cd32685ea8a5429fe557
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-05-24 10:40:21 +0200

    test: add async fetch to fix data?

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ import express, { response } from "express";
 const app = express();
 
 // URL data from REST API | fetch data als JSON -> variabele data
-const url = "https://cssday.nl/data.json";
-const data = await fetch(url).then((response) => response.json());
-console.log(data);
+// const url = "https://cssday.nl/data.json";
+// const data = await fetch(url).then((response) => response.json());
+// console.log(data);
+
+app.get("/", (request, response) => {
+  fetchJson("https://cssday.nl/data.json").then((data) => {
+    //fetch functie aanroepen en data ophalen
+    response.render("index", { data }); //data doorsturen en pagina laden
+    console.log(data);
+
+    // const allData = data;
+    // console.log(allData);
+
+    // const year2013 = data[2013];
+    // console.log(year2013);
+  });
+});
 
 // Instellen EJS | 'views' map doorgeven
 app.set("view engine", "ejs");
@@ -17,9 +31,9 @@ app.set("views", "./views");
 app.use(express.static("./public"));
 
 //  Index route maken
-app.get("/", function (req, res) {
-  res.render("index", data);
-});
+// app.get("/", function (req, res) {
+//   res.render("index", data);
+// });
 
 // Poortnummer instellen
 app.set("port", process.env.PORT || 8000);
@@ -30,4 +44,18 @@ app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
 
-// app.use(express.static("public"));
+export async function fetchJson(data) {
+  return await fetch(data)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
+
+// export async function postJson(url, body) {
+//   return await fetch(url, {
+//     method: "post",
+//     body: JSON.stringify(body),
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((response) => response.json())
+//     .catch((error) => error);
+// }
